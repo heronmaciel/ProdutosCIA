a603a6cef2271a8bf3f28a121dc483e8f15c0483
Heron de Rezende Maciel <heron.maciel@gmail.com>
2025-08-20 13:20:26 -0300

    Ajustes para o Subir o Swagger e apresentar em tela

--- ProdutosCIA/Program.cs
+++ ProdutosCIA/Program.cs
@@ -9,7 +9,7 @@ using Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers();
 
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
@@ -31,16 +31,21 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(connectionString);
 });
 
-builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
